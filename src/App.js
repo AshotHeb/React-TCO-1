8d89d74a8732c4e6a21eaad8539854fe7798053f
user <AshotHeb>
2022-04-26 20:24:04 +0400

    Removed unnecessary thing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { H1 } from "./H1/H1";
-import { P } from "./P/P";
-
-function Image() {
-  return (
-    <div>
-      <img
-        src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?cs=srgb&dl=pexels-lukas-rodriguez-3680219.jpg&fm=jpg"
-        alt="image"
-      />
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <div className="App" id="barev">
-      <div className="info-section">
-        <H1 />
-        <H1 />
-        <H1 />
-
-        <P />
-        <Image />
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
