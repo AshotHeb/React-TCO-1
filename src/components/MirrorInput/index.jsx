ccb143ca45f15b785b2d683e2563b6c8db110d49
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-06-02 21:58:59 +0400

    Redux

---
+++ src/components/MirrorInput/index.jsx
@@ -0,0 +1,14 @@
+import { connect } from "react-redux"
+export const MirrorInput = () => {
+    return (
+        <div>
+            <input type="text" />
+            <p>Result:</p>
+        </div>
+    )
+}
+// export const mapStateToProps = (state) => {
+//     return (
+        
+//     )
+// }
