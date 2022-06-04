0bf9c0c2141d47581fcf899085c915d41ae4e27a
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-06-04 14:59:16 +0400

    Redux connect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { RoutesComponent } from "./components/Routes";
 import { Header } from "./layout/Header";
+import { store } from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <RoutesComponent />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <RoutesComponent />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
